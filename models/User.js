8af90187d72bf597e1ad5f5105cf4b16f4adb2e1
seanpaulkelley <sean@horse101.com>
2017-10-10 15:57:20 -0400

    added google authentication without logout functionality

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+const { Schema } = mongoose; //es 2015 of the above line aka destructuring
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+//create model class
+mongoose.model('users', userSchema);
---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  //passport sees returned code
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
