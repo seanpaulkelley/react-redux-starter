6b30493cf12d251a21c6e0462084490a1995d494
seanpaulkelley <sean@horse101.com>
2017-10-11 17:17:51 -0400

    fixed keys file type for prod

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   facebookClientID: process.env.FACEBOOK_CLIENT_ID,
